7aeed29127890e92bff8a7c160a3550917dbc2b5
Halit Mesut <halitmesut@gmail.com>
2022-05-29 01:44:52 +0300

    first init

---
+++ android/app/src/main/kotlin/com/example/xylophonee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xylophonee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void playSound(int noteNumber) {
+    final player = AudioPlayer();
+    player.setAsset('assets/audios/note$noteNumber.wav');
+    player.play();
+  }
+
+  Expanded buildKey({required int soundNumber, required Color buttonColor}) {
+    return Expanded(
+      child: Container(
+        color: buttonColor,
+        child: TextButton(
+          onPressed: () {
+            playSound(soundNumber);
+          },
+          child: Text(""),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(soundNumber: 1, buttonColor: Colors.red),
+              buildKey(soundNumber: 2, buttonColor: Colors.orange),
+              buildKey(soundNumber: 3, buttonColor: Colors.yellow),
+              buildKey(soundNumber: 4, buttonColor: Colors.green),
+              buildKey(soundNumber: 5, buttonColor: Colors.blue),
+              buildKey(soundNumber: 6, buttonColor: Colors.indigo),
+              buildKey(soundNumber: 7, buttonColor: Colors.purple)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
